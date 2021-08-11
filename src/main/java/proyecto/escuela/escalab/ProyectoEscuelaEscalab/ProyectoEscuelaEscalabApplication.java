7633ec179377d8569240ae4587ab092e09399078
SaulSantis <saulssantis@gmail.com>
2021-08-10 22:41:19 -0400

    Initial commit

---
+++ src/main/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/ProyectoEscuelaEscalabApplication.java
@@ -0,0 +1,13 @@
+package proyecto.escuela.escalab.ProyectoEscuelaEscalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEscuelaEscalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEscuelaEscalabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/escuela/escalab/ProyectoEscuelaEscalab/ProyectoEscuelaEscalabApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.escuela.escalab.ProyectoEscuelaEscalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEscuelaEscalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
